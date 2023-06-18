7fc522fedffb6c06e0074c5473cb9883da93b671
KarthikBenki <karthikmc007@gmail.com>
2023-06-18 10:16:58 +0530

    render simple html tag on browser

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const port = 8000
 
 const server = http.createServer((req,res)=>{
     console.log(req.url);
+    res.writeHead(200,{'Content-Type':"text/html"})
     switch(req.url){
-        case '/about':res.end("about")
+        case '/about':res.end("<h1>Welcome</h1>")
         break;
         case "/login":res.end("login")
         break;
